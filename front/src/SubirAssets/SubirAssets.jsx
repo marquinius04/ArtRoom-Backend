40698dae81c66095b8da176a8f14614698d72b6e
marquinius04 <marcosiesmh@gmail.com>
2025-05-15 18:04:31 +0200

    Página de subida de assets completamente terminada

--- front/src/SubirAssets/SubirAssets.jsx
+++ front/src/SubirAssets/SubirAssets.jsx
@@ -7,6 +7,62 @@ import { useState } from "react";
 export const SubirAssets = ({ className, ...props }) => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [tags, setTags] = useState([]); // Estado para almacenar las tags
+  const [newTag, setNewTag] = useState(""); // Estado para la nueva tag que se va a agregar
+  const [uploadedFile, setUploadedFile] = useState(null); // Estado para almacenar la URL del archivo subido
+  const [uploadedFileName, setUploadedFileName] = useState(""); // Estado para almacenar el nombre del archivo subido
+  const [thumbnailFile, setThumbnailFile] = useState(null); // Estado para almacenar la URL del thumbnail
+  const [thumbnailFileName, setThumbnailFileName] = useState(""); // Estado para almacenar el nombre del thumbnail
+
+  const handleThumbnailUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileURL = URL.createObjectURL(file); // Crea una URL temporal para mostrar la imagen
+      setThumbnailFile(fileURL); // Actualiza el estado con la URL del archivo
+      setThumbnailFileName(file.name); // Actualiza el estado con el nombre del archivo
+      console.log("Thumbnail subido:", file.name);
+    }
+  };
+
+  const categories = [
+    "Animals and Plants",
+    "Realistic Furnitures",
+    "Weapons and Ammunition",
+    "Sculpture and Art",
+    "Fantastic Landscapes",
+    "Realistic Landscapes",
+    "Cars and Vehicles",
+    "Buildings and Structures",
+  ];
+
+  const availableCategories = categories.filter(
+    (category) => !tags.includes(category)
+  ); // Filtra las categorías que no están en las tags
+
+  const handleRemoveTag = (indexToRemove) => {
+    setTags(tags.filter((_, index) => index !== indexToRemove));
+  };
+
+  const handleAddTag = () => {
+    if (newTag.trim() !== "" && !tags.includes(newTag)) {
+      setTags([...tags, newTag.trim()]); // Agrega la nueva tag al estado
+      setNewTag(""); // Limpia el desplegable
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTag(e.target.value); // Actualiza el estado del desplegable
+  };
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileURL = URL.createObjectURL(file); // Crea una URL temporal para mostrar la imagen
+      setUploadedFile(fileURL); // Actualiza el estado con la URL del archivo
+      setUploadedFileName(file.name); // Actualiza el estado con el nombre del archivo
+      console.log("Archivo subido:", file.name);
+    }
+  };
 
   useEffect(() => {
     const user = localStorage.getItem("user"); // Obtiene el usuario del localStorage
@@ -35,21 +91,137 @@ export const SubirAssets = ({ className, ...props }) => {
           />
         </div>
         <div className="auth-buttons">
-          <button className="upload-icon"><img src="https://www.dropbox.com/scl/fi/o4cednhkybd1ty8xsp5x7/upload-icon.png?rlkey=0ymn2yz9rqdpuyf2hd50hoa7o&st=0t6y1zo8&dl&raw=1"></img></button>
-          <button className="user-icon"><img src="https://www.dropbox.com/scl/fi/hfz5wn581d6rot1ccxuyh/user-icon.png?rlkey=hm75yyttqaw7hb8n5tk3ja3xq&st=rknzoa1v&dl&raw=1"></img></button>
+          <button className="upload-icon">
+            <img src="https://www.dropbox.com/scl/fi/o4cednhkybd1ty8xsp5x7/upload-icon.png?rlkey=0ymn2yz9rqdpuyf2hd50hoa7o&st=0t6y1zo8&dl&raw=1"></img>
+          </button>
+          <button className="user-icon">
+            <img src="https://www.dropbox.com/scl/fi/hfz5wn581d6rot1ccxuyh/user-icon.png?rlkey=hm75yyttqaw7hb8n5tk3ja3xq&st=rknzoa1v&dl&raw=1"></img>
+          </button>
         </div>
       </div>
 
       <div className="subir-assets-container">
         <div className="div-izquierda">
-          <div className="main-info"></div>
-          <div className="media-files"></div>
-          {/* <button className="button">
-            <img src="https://www.dropbox.com/scl/fi/o4cednhkybd1ty8xsp5x7/upload-icon.png?rlkey=0ymn2yz9rqdpuyf2hd50hoa7o&st=0t6y1zo8&dl&raw=1"></img>
+          <div className="main-info">
+            <div className="input-container">
+              <label className="input-label">Title</label>
+              <input
+                type="text"
+                className="input-field"
+                placeholder="What is the name of your project?"
+              />
+            </div>
+            <div className="input-container">
+              <label className="input-label">Description</label>
+              <input
+                type="text"
+                className="input-field"
+                placeholder="Describe your project"
+              />
+            </div>
+          </div>
+          <div className="media-files">
+            {uploadedFile ? (
+              <>
+                <img
+                  src={uploadedFile}
+                  alt="Uploaded file preview"
+                  className="uploaded-file-preview"
+                />
+                <p className="uploaded-file-name">{uploadedFileName}</p>
+              </>
+            ) : (
+              <>
+                <input
+                  type="file"
+                  id="media-upload"
+                  className="file-input"
+                  accept=".mp4, .png, .jpg, .jpeg, .obj, .fbx, .glb, .gltf"
+                  onChange={(e) => handleFileUpload(e)}
+                />
+                <label htmlFor="media-upload" className="media-upload-label">
+                  <img
+                    src="https://www.dropbox.com/scl/fi/msdm8luh3afl9g9qwl96o/media-upload.png?rlkey=ws5ahcdoipiueocleyrqsyolj&st=n3k2l30l&raw=1"
+                    alt="media upload icon"
+                  />
+                </label>
+                <p>Add or drag any type of media file here</p>
+              </>
+            )}
+          </div>
+          <button className="button">
+            <img
+              src="https://www.dropbox.com/scl/fi/vvz488011glv9otp4gtxa/upload-icon-alt.png?rlkey=5kyfsnfje3jcymlneko1wxmue&st=qz61etu6&raw=1"
+              alt="upload alternative icon"
+              className="upload-icon"
+            />
             Upload
-          </button> */}
+          </button>
         </div>
         <div className="div-derecha">
+          <h1>Thumbnail</h1>
+          <div className="media-rectangle">
+            {thumbnailFile ? ( // Cambiado de uploadedFileName a thumbnailFile
+              <img
+                src={thumbnailFile} // Cambiado de uploadedFile a thumbnailFile
+                alt="Uploaded thumbnail preview"
+                className="uploaded-file-preview"
+              />
+            ) : (
+              <>
+                <input
+                  type="file"
+                  id="thumbnail-upload"
+                  className="file-input"
+                  accept=".png, .jpg, .jpeg"
+                  onChange={(e) => handleThumbnailUpload(e)}
+                />
+                <label htmlFor="thumbnail-upload" className="media-upload-label">
+                  <img
+                    src="https://www.dropbox.com/scl/fi/18y0fpk4tvf1rhxzt93gq/media-icon.svg?rlkey=wph40wpuhvkgxajjbd0441mxm&st=c2cuud3q&raw=1"
+                    alt="media file icon"
+                  />
+                </label>
+              </>
+            )}
+          </div>
+          <button className="button" onClick={() => document.getElementById("thumbnail-upload").click()}>
+            Upload photo
+          </button>
+          <h1>Tags</h1>
+          <div className="tags-container">
+            {tags.map((tag, index) => (
+              <div key={index} className="tag">
+                {tag}
+                <button
+                  className="remove-tag-button"
+                  onClick={() => handleRemoveTag(index)}
+                >
+                  ✖
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="add-tag-container">
+            <select
+              className="add-tag-select"
+              value={newTag}
+              onChange={handleInputChange}
+            >
+              <option value="">Select a category...</option>
+              {availableCategories.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <button className="add-tag-button" onClick={handleAddTag}>
+              <img
+                src="https://www.dropbox.com/scl/fi/w9f8fad221ofvffhg9im6/add-tag-button.svg?rlkey=30vbxlaff4cnscrajjj8f0otk&st=wgrj2bef&raw=1"
+                alt="add tag button"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
